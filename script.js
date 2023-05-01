d8040bcf2bf52ebe3420e71f806fb76744cd75b4
Joao Victor <joaovictorrodrigues444@gmail.com>
2023-04-30 22:35:37 -0300

    FIX bug

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ const display = document.querySelector("#display")
 
 function insert(num) {
 
-  display.textContent += num
-  const value = display.textContent
+  display.value += num
+  const value = display.value
   const fistChar = value.charAt(0)
-  const lastChar = value.split(-1)
-  
-  if(isNaN(fistChar) || isNaN(lastChar)) {
-    display.textContent = null
+  const lastChar = value.charAt(value.length - 1)
+  const penultimateChar = value.charAt(value.length - 2)
+
+  if(isNaN(fistChar)) {
+    display.value = null
+  }
+
+  if(isNaN(lastChar) && isNaN(penultimateChar)) {
+    display.value = display.value.slice(0, - 1)
   }
 }
 
 function calcular() {
-  let result = display.textContent
-  display.textContent = eval(result)
+  let result = display.value
+  display.value = eval(result)
 }
 
 function clean() {
-  display.textContent = ""
+  display.value = ""
 }
 
 function back() {
-  let resultado = display.textContent;
-  display.textContent = resultado.substring(0, resultado.length -1);
+  let resultado = display.value;
+  display.value = resultado.substring(0, resultado.length -1);
 }
